504822e16380818ade37639184e6ac3390608fe6
GPrielipp <gwprielipp@gmail.com>
2022-04-28 21:27:44 -0800

    Added CompositeMultiplyNode. Represents the multiplication of two nodes

---
+++ inc/Node/CompositeMultiplyNode.hpp
@@ -0,0 +1,20 @@
+#ifndef COMPOSITE_MULTIPLY_NODE_HPP
+#define COMPOSITE_MULTIPLY_NODE_HPP
+
+#include "./ComponentNode.hpp"
+
+class CompositeMultiplyNode : public ComponentNode
+{
+private:
+    ComponentNode* leftChild;
+    ComponentNode* rightChild;
+public:
+    CompositeMultiplyNode(ComponentNode* left, ComponentNode* right);
+    ~CompositeMultiplyNode();
+    NodeTypes item();       //return NodeTypes::BINARY
+    ComponentNode* left();  //return this->leftChild
+    ComponentNode* right(); //return this->rightChild
+    void accept(Visitor &visitor);
+};
+
+#endif//COMPOSITE_MULTIPLY_NODE_HPP
