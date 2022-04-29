bcc117ee0ab035a1aa1a8bdd22e85541da4c697a
GPrielipp <gwprielipp@gmail.com>
2022-04-28 21:21:28 -0800

    Added Composite Negate Node (turning a +# --> -# or -# --> +#)

---
+++ inc/Node/CompositeNegateNode.hpp
@@ -0,0 +1,19 @@
+#ifndef COMPOSITE_NEGATE_NODE_HPP
+#define COMPOSITE_NEGATE_NODE_HPP
+
+#include "./ComponentNode.hpp"
+
+class CompositeNegateNode : public ComponentNode
+{
+private:
+    ComponentNode* negatedNode;
+public:
+    CompositeNegateNode(ComponentNode* negatedNode);
+    ~CompositeNegateNode();
+    NodeTypes item();       //return NodeTypes::UNARY
+    ComponentNode* left();  //return NO_NODE
+    ComponentNode* right(); //return this->negatedNode
+    void accept(Visitor &visitor);
+};
+
+#endif//COMPOSITE_NEGATE_NODE_HPP
